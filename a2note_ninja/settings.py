297b2cf0cda4912e55209a20f3652af124abbd18
alessandro-amadelli <alessandro.amadelli@gmail.com>
2021-05-28 19:25:46 +0200

    Desperately trying to deactivate DEBUG mode and hoping that it keeps working

--- a2note_ninja/settings.py
+++ a2note_ninja/settings.py
@@ -30,7 +30,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = os.environ['A2NOTE_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
