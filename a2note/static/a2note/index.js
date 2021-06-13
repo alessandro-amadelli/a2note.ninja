92cfeb1a8ca44a8fee941b3c72c8c7142c186d71
alessandro-amadelli <alessandro.amadelli@gmail.com>
2021-06-13 12:55:21 +0200

    Added modal for list deletion confirmation before actually deleting the list. Added filter in index page so an authenticated user can filter the shown lists by type.

--- a2note/static/a2note/index.js
+++ a2note/static/a2note/index.js
@@ -5,8 +5,43 @@ document.addEventListener('DOMContentLoaded', function(){
     showInfo(this.parentElement.parentElement);
   });
 
+  initializeRadioBtn();
+
 });
 
 function showInfo(item) {
   item.querySelector(".info-text").classList.toggle("hidden");
+}
+
+function initializeRadioBtn(){
+  let radios = document.querySelectorAll(`input[type="radio"]`);
+
+  radios.forEach((radio, i) => {
+    radio.onclick = function() {
+      filterLists(this);
+    }
+  });
+
+}
+
+function filterLists(radio) {
+  if (radio.checked) {
+    let val = radio.value;
+
+    document.querySelectorAll(".list-thumbnail").forEach((thumbnail, i) => {
+      if (val == "ALL") {
+        thumbnail.classList.remove("hidden");
+      } else {
+        let type = thumbnail.dataset.type;
+        if (type == val) {
+          thumbnail.classList.remove("hidden");
+        } else {
+          thumbnail.classList.add("hidden");
+        }
+      }
+    });
+
+
+
+  }
 }
