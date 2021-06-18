5e76511aa1a7253edb2a6a0ee3192d246e2fa762
alessandro-amadelli <alessandro.amadelli@gmail.com>
2021-06-18 17:36:06 +0200

    Fixed link to send via mail

--- a2note/static/a2note/shoplist_editor.js
+++ a2note/static/a2note/shoplist_editor.js
@@ -81,6 +81,9 @@ function initializeShareBtns() {
   btnTelegram.href = "https://telegram.me/share/url?url=" + window.location.href + "&text=" + text;
   //<a href="tg://msg_url?url=https://valid.url&amp;text=text">Telegram</a>
 
+  let bntEmail = document.querySelector("#btnEmail");
+  btnEmail.href = "mailto:?subject='Shopping list'&body=" + text + window.location.href;
+
 }
 
 function btnSaveClick(reload=false) {
