624bc3bb768e5bb56c524e8ff11301e893438e98
alessandro-amadelli <alessandro.amadelli@gmail.com>
2022-04-20 11:57:00 +0200

    change: Additional user info on administration page are now hidden and can be revealed by clicking on the username cell of the table

--- a2note/static/a2note/admin_page.js
+++ a2note/static/a2note/admin_page.js
@@ -1,21 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-
     //Set the onclick property on each block/activate user button
     document.querySelectorAll(".btnBlock").forEach(btn => {
         btn.onclick = function(){
             showLoading("Please wait...");
             btn.setAttribute("class", "btn btn-warning");
-            btn.innerHTML = `
-            <span class='material-icons-outlined'>sync</span> Changed
-            `;
+            btn.innerHTML = `<span class='material-icons-outlined'>sync</span> Changed`;
             btn.disabled = true;
             blockUser(btn.dataset.action, btn.dataset.username);
         }
     });
+
+    document.querySelectorAll(".triggerUserInfo").forEach(element => {
+        element.onclick = () => {
+            element.querySelector("table").classList.toggle("hidden");
+        };
+    });
 });
 
 function blockUser(action, username){
-    //alert("Action: " + action + "; Username: " + username);
     var csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
     const request = new XMLHttpRequest();
     const url = `/admin_block_user/`;
